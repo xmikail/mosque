5dcffa93b468eb9f019ba849f89af780eb9be564
Mikail Khalkov <mkhalikov@gmail.com>
2017-06-10 01:12:41 +0300

    изменения в gulpfile, незначительные изменения в стилях, в index.html imam img и caption изменен на imam_card_img imam_card_caption

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp');
+    watch = require('gulp-watch');
 
-gulp.task('default', function() {
-    
+gulp.task('default', 'stream', function() {
+    return watch('styles/*.css', { ignoreInitial: false })
+        .pipe(gulp.dest('build'));
 });
+
+gulp.task('callback', function () { 
+    return watch('styles/*.css', function () { 
+        gulp.src('styles/*.css')
+            .pipe(gulp.dest('build'));
+    });
+});
+
+gulp.task('default', () => 
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/'))
+);
